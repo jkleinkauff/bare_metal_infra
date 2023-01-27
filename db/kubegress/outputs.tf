f92efccf98d948bd3485fed49f742158c8e8ffb2
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2023-01-26 21:30:35 -0300

    add module

---
+++ db/kubegress/outputs.tf
@@ -0,0 +1,9 @@
+output "db_user" {
+  description = "Database user"
+  value       = var.db_user
+}
+
+output "db_password" {
+  description = "Database password"
+  value       = var.db_password
+}
