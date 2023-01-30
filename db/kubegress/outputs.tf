6a5579b508b4b482b855f40ce764312c61327256
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2023-01-29 21:31:15 -0300

    fixes

--- db/kubegress/outputs.tf
+++ db/kubegress/outputs.tf
@@ -1,3 +1,8 @@
+output "db_name" {
+  description = "Database name"
+  value       = var.db_name
+}
+
 output "db_user" {
   description = "Database user"
   value       = var.db_user
@@ -6,4 +11,9 @@ output "db_user" {
 output "db_password" {
   description = "Database password"
   value       = var.db_password
+}
+
+output "db_host" {
+  description = "Database host"
+  value       = "${var.name}-pg-db.${var.namespace}"
 }
