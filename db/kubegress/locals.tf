319c26dd7dd6777abd85141ddc9e222f346cd644
jkleinkauff <jhonatas.kleinkauff@loadsmart.com>
2023-02-12 15:05:29 -0300

    output replica pg host

--- db/kubegress/locals.tf
+++ db/kubegress/locals.tf
@@ -3,5 +3,6 @@ locals {
   db_user                 = var.db_user
   db_password             = var.db_password
   db_host                 = "${var.name}-pg-db.${var.namespace}"
+  db_host_replica         = "${var.name}-pg-db-replica.${var.namespace}"
   airbyte_connection_name = "${var.db_name}-airbyte-connection"
 }
